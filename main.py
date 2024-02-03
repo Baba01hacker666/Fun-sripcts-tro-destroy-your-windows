b3a396a8976dfee593ba9e1b74b23cae17039cf0
Baba01hacker666 <117832562+Baba01hacker666@users.noreply.github.com>
2024-02-03 14:07:29 +0545

    Update main.py
    Fixing bugs

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
-
-while True:
+print("this will destroy your filesystem or make it unstable ")
+a = input("type y to continue:")
+if a=="y":
+ while True:
     # Create a directory
-    os.mkdir('...')
+     os.mkdir('...')
     
     # Change into the directory
-    os.chdir('...')
+     os.chdir('...')
 
